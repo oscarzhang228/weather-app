907d66cb164bb65575b7fcdd76608c80ba84fd97
Oscar Zhang <oscarzhang228@gmail.com>
2023-01-08 21:54:06 -0500

    display information on card and added a picture to the card based on weather currently happening

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const locationButton = document.getElementById("locationBtn");
 const locations = document.getElementById("location");
 const spinner = document.getElementById("loading");
 const state = document.getElementById("state");
+const card = document.getElementById("theCard");
+card.style.display = "none";
 //probably shouldn't do this but prob won't use it that much
 const apiKey = "ba019c1ac4809c860e9857f1d38f39c7";
 spinner.style.display = "none";
@@ -35,9 +37,23 @@ locationButton.addEventListener("click", async () => {
       })
   );
   const weatherJson = await weather.json();
-  // in kelvin
-  const temperature = weatherJson.main.temp;
+  let temperature = 1.8 * (weatherJson.main.temp - 273) + 32;
+  temperature = Math.round(temperature);
   const description = weatherJson.weather[0].main;
   console.log(temperature + " " + description);
-  //take this, add picture weather, and temperature to card and remove loading
+  const date = new Date();
+  let time = date.getHours() + ":" + date.getMinutes();
+  const cardTime = document.getElementById("cardTime");
+  cardTime.textContent = time;
+  const cardTemp = document.getElementById("cardTemp");
+  cardTemp.textContent = temperature + "\u00B0";
+  const cardDescription = document.getElementById("cardDescription");
+  cardDescription.textContent = description;
+  const cardLocation = document.getElementById("cardLocation");
+  cardLocation.textContent =
+    locations.value.charAt(0).toUpperCase() + locations.value.slice(1);
+  const cardImg = document.getElementById("cardPic");
+  cardImg.src = `./images/${description}.png`;
+  card.style.display = "flex";
+  spinner.style.display = "none";
 });
